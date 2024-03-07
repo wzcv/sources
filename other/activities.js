01de7d136e54ff179a90e15f2c024dc1e3d16e87
Zhichen Wu <qhwuzhichen@126.com>
2024-03-07 16:56:31 +0800

    Update activities.js

--- other/activities.js
+++ other/activities.js
@@ -197,15 +197,15 @@ async function getxz() {
 
 async function task1() {
     console.log("临时任务开始")
-    let ids = await axios.get("https://mirror.ghproxy.com/https://raw.githubusercontent.com/Wenmoux/sources/master/other/id.json");
-    for (id of ids.data) {
-        result += await jhy(id) + "\n"
-    }
-    let ids2 = await axios.get("https://mirror.ghproxy.com/https://raw.githubusercontent.com/Wenmoux/sources/master/other/id2.json");
-    for (id of ids2.data) {
-        await ddd(id)
-    }
-    await lottery("lottery","0",49)
+   // let ids = await axios.get("https://mirror.ghproxy.com/https://raw.githubusercontent.com/Wenmoux/sources/master/other/id.json");
+   // for (id of ids.data) {
+   //     result += await jhy(id) + "\n"
+   // }
+   // let ids2 = await axios.get("https://mirror.ghproxy.com/https://raw.githubusercontent.com/Wenmoux/sources/master/other/id2.json");
+   // for (id of ids2.data) {
+    //    await ddd(id)
+   // }
+  //  await lottery("lottery","0",49)
  //   await slm()
     await glist(3)
     await getxz()
